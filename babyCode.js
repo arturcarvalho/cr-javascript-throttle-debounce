8cfe6fd4d46bbe8f60c7648346f0d95d43f5601f
Artur Carvalho <arturbelico@gmail.com>
2019-07-26 09:08:45 +0200

    Refactor

---
+++ babyCode.js
@@ -0,0 +1,27 @@
+const touchme = document.getElementById("touchme");
+
+const babyBucket = document.getElementById("baby-bucket");
+const dadBucket = document.getElementById("dad-bucket");
+const momBucket = document.getElementById("mom-bucket");
+
+function addDrop() {
+  const drop = document.createElement("img");
+  drop.src = "assets/iconmonstr-drop-6.svg";
+  babyBucket.appendChild(drop);
+}
+
+function askDone() {
+  const doneYet = document.createElement("div");
+  doneYet.innerText = "Did he finish?";
+  dadBucket.appendChild(doneYet);
+}
+
+function putDiaper() {
+  const diaper = document.createElement("img");
+  diaper.src = "assets/noun_diaper_1549135.svg";
+  momBucket.appendChild(diaper);
+}
+
+touchme.addEventListener("mousemove", addDrop);
+touchme.addEventListener("mousemove", askDone);
+touchme.addEventListener("mousemove", putDiaper);
---
+++ commonCode.js
@@ -0,0 +1,8 @@
+
+function throttle() {
+  console.log("throttling")
+}
+
+function debounce() {
+  console.log("debounce")
+}
